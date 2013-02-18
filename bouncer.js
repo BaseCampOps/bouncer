7e6b94fd4b6e42fec0b7995b781455e0530efe22
Paul Smith <catalystmediastudios@gmail.com>
2013-02-18 16:43:58 -0700

    Adds feature detection and functions to call when cant drag out

--- bouncer.js
+++ bouncer.js
@@ -1,11 +1,20 @@
-$(document).on('dragstart', '[data-downloadurl]', function (e) {
-  console.log('Started dragging');
-  if (!!window.FileReader) {
-    var elementBeingDragged = $(this);
-    console.log('Dataset', this.dataset.downloadurl);
-    console.log('Event', e);
-    if (this.dataset) {
-      e.originalEvent.dataTransfer.setData('DownloadURL', this.dataset.downloadurl);
-    }
-  };
-});
+var Bouncer = {};
+Bouncer.cantDragOut = function(){};
+Bouncer.canDragOut = function(){};
+
+(function($){
+  $(document).on('dragstart', '[data-downloadurl]', function (e) {
+    if (!!window.FileReader) {
+      var elementBeingDragged = $(this);
+      var dataTransfer = e.originalEvent.dataTransfer;
+      if (this.dataset && this.dataset.downloadurl) {
+        dataTransfer.setData('DownloadURL', this.dataset.downloadurl);
+      }
+      if (dataTransfer.items && dataTransfer.items.length > 0) {
+        Bouncer.canDragOut();
+      } else {
+        Bouncer.cantDragOut();
+      }
+    };
+  });
+})(jQuery);
